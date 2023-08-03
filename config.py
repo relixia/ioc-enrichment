77214292424353bbb6ff7de9df431086a5925a61
Buğra Çayır <bugra-net@hotmail.com>
2023-08-03 17:42:09 +0300

    dynaconf and envs are added as security practices

---
+++ config.py
@@ -0,0 +1,13 @@
+from dynaconf import Dynaconf
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+settings = Dynaconf(     
+    load_dotenv=True,
+    envvar_prefix_for_dynaconf=False,     
+    dotenv_path=str(BASE_DIR.joinpath("ioc-enrichment", "envs", ".env")),
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
--- src/tasks.py
+++ src/tasks.py
@@ -1,5 +1,19 @@
 from celery_base import app
 
+# 
+# 
+'''
+Kendi phishing servisim --> USOM, PhishTank, PhishStats, OpenPhish
+VirusTotal: Dosya tarama, URL analizi, IP ve alan adı istihbaratı sağlayan bir hizmet.
+Google reklamlar: https://adstransparency.google.com/?region=anywhere
+Shodan: İnternet üzerindeki cihazlar için açık port ve servis bilgisi sağlayan bir hizmet.
+AbuseIPDB: Zararlı IP adresleri hakkında bilgi sunan bir hizmet.
+IPinfo: IP adresleri için coğrafi konum ve whois bilgileri sağlayan bir hizmet.
+GreyNoise: İnternet gürültüsü ve zararlı IP adresleri arasındaki farkı belirleyen bir hizmet.
+AlienVault OTX: Ağ trafiğini izleyen ve zararlı davranışları algılayan açık tehdit istihbaratı platformu.
+URLhaus: Zararlı URL'leri içeren bir veritabanı.
+'''
+
 @app.task
 def say_hello(name: str):
     return f"Hello {name}"
