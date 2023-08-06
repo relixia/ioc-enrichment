2129eeae6f0da0164bdbcf8af04ddee61ffc8df6
Buğra Çayır <bugra-net@hotmail.com>
2023-08-07 02:15:57 +0300

    result page is constructed

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI, Request, Form, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from celery_base import app
-from tasks import virustotal_url, virustotal_file, virustotal_ip, virustotal_domain, ipinfo, abuseipdb, greynoise, opswat, opswat_file_reputation, kaspersky_file, kaspersky_ip, kaspersky_domain, kaspersky_url, hybridana_file, urlscanio, criminalip_ip, criminalip_domain, cloudflare_email, cloudflare_ip, iplocation, urlhaus, phishtank, usom, openphish, shodan, ipqualityscore, ipqualityscore_email, abstract_email, hunterio  
+from tasks import (
+    virustotal_url, virustotal_file, virustotal_ip, virustotal_domain, ipinfo, abuseipdb, greynoise, opswat, opswat_file_reputation,
+    kaspersky_file, kaspersky_ip, kaspersky_domain, kaspersky_url, hybridana_file, urlscanio, criminalip_ip, criminalip_domain,
+    cloudflare_email, cloudflare_ip, iplocation, urlhaus, phishtank, usom, openphish, shodan, ipqualityscore, ipqualityscore_email,
+    abstract_email, hunterio
+)
 import uvicorn
 import jinja2
 from utilities import check_input_type, calculate_file_hash, ioc_save_db
@@ -14,6 +19,7 @@ from models import Base, Session, IOC, engine
 app_fastapi = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+templates.env.globals.update(enumerate=enumerate)
 app_fastapi.mount("/static", StaticFiles(directory="templates/static"), name="static")
 
 
@@ -37,42 +43,42 @@ async def search(
         print("Input Type:", input_type)
         ioc_save_db(input_text, input_type)
         if input_type == "URL":
-            #virustotal_url.delay(input_text)
-            #kaspersky_url.delay(input_text)
-            #urlscanio.delay(input_text)
-            #cloudflare_email.delay(input_text)
-            #urlhaus.delay(input_text)
-            #phishtank.delay(input_text)
-            #openphish.delay(input_text)
+            virustotal_url.delay(input_text)
+            kaspersky_url.delay(input_text)
+            urlscanio.delay(input_text)
+            cloudflare_email.delay(input_text)
+            urlhaus.delay(input_text)
+            phishtank.delay(input_text)
+            openphish.delay(input_text)
             ipqualityscore.delay(input_text)
         elif input_type == "Domain":
-            #virustotal_domain.delay(input_text)
-            #kaspersky_domain.delay(input_text)
-            #criminalip_domain.delay(input_text)
-            #cloudflare_email.delay(input_text)
+            virustotal_domain.delay(input_text)
+            kaspersky_domain.delay(input_text)
+            criminalip_domain.delay(input_text)
+            cloudflare_email.delay(input_text)
             usom.delay(input_text)
             ipqualityscore.delay(input_text)
         elif input_type == "File Hash":
-            #virustotal_file.delay(input_text)
-            #opswat.delay(input_text)
-            #opswat_file_reputation.delay(input_text)
-            #kaspersky_file.delay(input_text)
+            virustotal_file.delay(input_text)
+            opswat.delay(input_text)
+            opswat_file_reputation.delay(input_text)
+            kaspersky_file.delay(input_text)
             hybridana_file.delay(input_text)
             cloudflare_email.delay(input_text)
         elif input_type == "IP Address":
-            #virustotal_ip.delay(input_text)
-            #ipinfo.delay(input_text)
-            #abuseipdb.delay(input_text)
-            #greynoise.delay(input_text)
-            #kaspersky_ip.delay(input_text)
-            #criminalip_ip.delay(input_text)
-            #cloudflare_ip.delay(input_text)
-            #cloudflare_email.delay(input_text)
+            virustotal_ip.delay(input_text)
+            ipinfo.delay(input_text)
+            abuseipdb.delay(input_text)
+            greynoise.delay(input_text)
+            kaspersky_ip.delay(input_text)
+            criminalip_ip.delay(input_text)
+            cloudflare_ip.delay(input_text)
+            cloudflare_email.delay(input_text)
             iplocation.delay(input_text)
             shodan.delay(input_text)
         elif input_type == "Email Address":
-            #ipqualityscore_email.delay(input_text)
-            #abstract_email.delay(input_text)
+            ipqualityscore_email.delay(input_text)
+            abstract_email.delay(input_text)
             hunterio.delay(input_text)
 
     elif input_file is not None:
@@ -83,8 +89,8 @@ async def search(
         sha256_hash = calculate_file_hash(file_content)
         print("SHA-256 Hash:", sha256_hash)
         ioc_save_db(sha256_hash, "File/File Hash")
-        #virustotal_file.delay(sha256_hash)
-        #opswat.delay(sha256_hash)
+        virustotal_file.delay(sha256_hash)
+        opswat.delay(sha256_hash)
         opswat_file_reputation.delay(sha256_hash)
         kaspersky_file.delay(sha256_hash)
         hybridana_file.delay(sha256_hash)
@@ -92,13 +98,28 @@ async def search(
     else:
         sha256_hash = None
 
+    # Fetch data from the database for the input_text or sha256_hash
+    session = Session()
+    if input_text:
+        result = session.query(IOC).filter_by(ioc=input_text).first()
+    elif sha256_hash:
+        result = session.query(IOC).filter_by(ioc=sha256_hash).first()
+    else:
+        result = None
+    session.close()
+
+    # Generate the list of services dynamically
+    services = [column.name for column in IOC.__table__.columns if column.name != "ioc" and column.name != "ioc_type"]
+
     return templates.TemplateResponse(
         "result.html",
         {
             "request": request,
             "input_text": input_text,
             "input_type": input_type,
-            "sha256_hash": sha256_hash
+            "sha256_hash": sha256_hash,
+            "result": result,
+            "services": services
         }
     )
 
